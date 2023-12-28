0ee7d5316a53b1ad3a1a6a05b9418acbcd9db10c
Tai <5506726tai@gmail.com>
2023-12-28 21:21:34 +0700

    ex finish

--- src/slices/messageBoardSlice.jsx
+++ src/slices/messageBoardSlice.jsx
@@ -1,1 +1,29 @@
 // TODO: Start Create Slice Here
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  messageText: "",
+  messageTextArray: [],
+};
+
+export const messageBoardSlice = createSlice({
+  name: "messageBoard",
+  initialState: initialState,
+  reducers: {
+    createMessageText: (state, action) => {
+      state.messageText = action.payload;
+    },
+    addMessageText: (state) => {
+      state.messageTextArray.push(state.messageText);
+      state.messageText = "";
+    },
+    deleteMessageText: (state, action) => {
+      state.messageTextArray.splice(action.payload, 1);
+    },
+  },
+});
+// export action ไปใช่กับปุ่ม
+export const { createMessageText, addMessageText, deleteMessageText } =
+  messageBoardSlice.actions;
+//export state ไปใส่ store
+export default messageBoardSlice.reducer;
